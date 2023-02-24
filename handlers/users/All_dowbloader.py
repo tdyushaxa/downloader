2518c4e47c890ac8f966271939a945e3159eff67
tdyushaxa <smaxmudov015@gmail.com>
2023-02-24 10:05:31 +0500

    update youtube

--- handlers/users/All_dowbloader.py
+++ handlers/users/All_dowbloader.py
@@ -21,16 +21,48 @@ async def downloader(message:types.Message):
         await message.answer(lookup['title'])
 
 
-@dp.message_handler(Text(startswith="https://youtu"))
-async def Yuotube_downloader(message:types.Message):
-    link=message.text
-    buffer=io.BytesIO()
-    downl = YouTube(link).streams.get_by_itag(itag=18)
-    downl.stream_to_buffer(buffer=buffer)
-    buffer.seek(0)
-    file_name=downl.title
-    await message.answer(f'{message.from_user.full_name} video yuklanmoqda...')
-    await message.answer_video(video=buffer,caption=file_name)
+@dp.message_handler()
+async def cmd_answer(message: types.Message):
+    if message.text.startswith('https://youtube.be/') or message.text.startswith(
+            'https://www.youtube.com/') or message.text.startswith('https://youtu.be/'):
+        url = message.text
+        yt = YouTube(url)
+        title = yt.title
+        author = yt.author
+        channel = yt.channel_url
+        resolution = yt.streams.get_highest_resolution().resolution
+        file_size = yt.streams.get_highest_resolution().filesize
+        length = yt.length
+        date_published = yt.publish_date.strftime("%Y-%m-%d")
+        views = yt.views
+        picture = yt.thumbnail_url
+
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(types.InlineKeyboardButton(text="yuklash", callback_data="download"))
+        await message.answer_photo(f'{picture}', caption=f"📹 <b>{title}</b> <a href='{url}'>→</a> \n"
+                                                         f"👤 <b>{author}</b> <a href='{channel}'>→</a> \n"
+                                                   ,
+                                   parse_mode='HTML', reply_markup=keyboard)  # Views#
+    else:
+        await message.answer(f"❗️<b>yuklab bo'lmadi я!</b>", parse_mode='HTML')
+
+
+@dp.callback_query_handler(text="download")
+async def button_download(call: types.CallbackQuery):
+    url = call.message.html_text
+    yt = YouTube(url)
+    title = yt.title
+    author = yt.author
+    resolution = yt.streams.get_highest_resolution().resolution
+    stream = yt.streams.filter(progressive=True, file_extension="mp4")
+    stream.get_highest_resolution().download(f'{call.message.chat.id}', f'{call.message.chat.id}_{yt.title}')
+    with open(f"{call.message.chat.id}/{call.message.chat.id}_{yt.title}", 'rb') as video:
+        await bot.send_video(call.message.chat.id, video, caption=f"📹 <b>{title}</b> \n"  # Title#
+                                                                  f"👤 <b>{author}</b> \n\n",  # Author Of Channel#
+                             parse_mode='HTML')
+        os.remove(f"{call.message.chat.id}/{call.message.chat.id}_{yt.title}")
+
+
 
 
 @dp.message_handler(Text(startswith="https://www.instagram.com"))
