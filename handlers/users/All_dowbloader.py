a020ef65a7a4cc42c206421a9983a5ed3a3ef6da
tdyushaxa <smaxmudov015@gmail.com>
2023-02-24 11:09:21 +0500

    changed files one

--- handlers/users/All_dowbloader.py
+++ handlers/users/All_dowbloader.py
@@ -25,44 +25,22 @@ async def downloader(message:types.Message):
 async def cmd_answer(message: types.Message):
     if message.text.startswith('https://youtube.be/') or message.text.startswith(
             'https://www.youtube.com/') or message.text.startswith('https://youtu.be/'):
-        url = message.text
         yt = YouTube(url)
         title = yt.title
         author = yt.author
-        channel = yt.channel_url
         resolution = yt.streams.get_highest_resolution().resolution
-        file_size = yt.streams.get_highest_resolution().filesize
-        length = yt.length
-        date_published = yt.publish_date.strftime("%Y-%m-%d")
-        views = yt.views
-        picture = yt.thumbnail_url
-
-        keyboard = types.InlineKeyboardMarkup()
-        keyboard.add(types.InlineKeyboardButton(text="yuklash", callback_data="download"))
-   
-        await message.answer_photo(f'{picture}', caption=f"📹 <b>{title}</b> <a href='{url}'>→</a> \n"
-                                                         f"👤 <b>{author}</b> <a href='{channel}'>→</a> \n"
-                                                   ,
-                                   parse_mode='HTML', reply_markup=keyboard)  # Views#
-    else:
-        await message.answer(f"❗️<b>yuklab bo'lmadi!</b>", parse_mode='HTML')
+        stream = yt.streams.filter(progressive=True, file_extension="mp4")
+        stream.get_highest_resolution().download(f'{call.message.chat.id}', f'{call.message.chat.id}_{yt.title}')
+        with open(f"{call.message.chat.id}/{call.message.chat.id}_{yt.title}", 'rb') as video:
+            await bot.send_video(call.message.chat.id, video, caption=f"📹 <b>{title}</b> \n"  # Title#
+                                                                    f"👤 <b>{author}</b> \n\n",  # Author Of Channel#
+                                parse_mode='HTML')
+            os.remove(f"{call.message.chat.id}/{call.message.chat.id}_{yt.title}")
 
 
-@dp.callback_query_handler(text="download")
-async def button_download(call: types.CallbackQuery):
-    url = call.message.html_text
-    yt = YouTube(url)
-    title = yt.title
-    author = yt.author
-    resolution = yt.streams.get_highest_resolution().resolution
-    stream = yt.streams.filter(progressive=True, file_extension="mp4")
-    stream.get_highest_resolution().download(f'{call.message.chat.id}', f'{call.message.chat.id}_{yt.title}')
-    with open(f"{call.message.chat.id}/{call.message.chat.id}_{yt.title}", 'rb') as video:
-        await message.answer(f"❗️<b>Yulanmoqdaa...</b>", parse_mode='HTML')
-        await bot.send_video(call.message.chat.id, video, caption=f"📹 <b>{title}</b> \n"  # Title#
-                                                                  f"👤 <b>{author}</b> \n\n",  # Author Of Channel#
-                             parse_mode='HTML')
-        os.remove(f"{call.message.chat.id}/{call.message.chat.id}_{yt.title}")
+
+
+    
 
 
 
